66bb8ac26201a971efe57e30af951c9e2b233fb9
omkar213 <omkardhumal213@gmail.com>
2022-01-12 15:21:16 +0530

    second commit by omkar dhumal

--- homepage.js
+++ homepage.js
@@ -1,20 +1,45 @@
-const divisions = document.querySelector('div');
-const navLi = document.querySelector('nav .navigation-bar ul li');
+var lastId,
+ topMenu = $("#mainNav"),
+ topMenuHeight = topMenu.outerHeight()+1,
+ // All list items
+ menuItems = topMenu.find("a"),
+ // Anchors corresponding to menu items
+ scrollItems = menuItems.map(function(){
+   var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+ });
 
-window.addEventListener('scroll', () => {
-    let currentdiv = '';
-    // console.log(scrollY); //This is used to console log the scrolling height of divisions
-    //offsetTop is used to read the scrolling
-    //clientHeight is used to get the height of each div
-    divisions.forEach(divsion => {
-        const divsionTop = divsion.offsetTop;   
-        const divsionHeight = divsion.clientHeight;
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 850);
+  e.preventDefault();
+});
 
-        if(scrollY >= divsionTop){
-            currentdiv = divsion.getAttribute('id');
-        }
-    })
-    navLi.forEach( li => {
-        li.class
-    })
-})
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
